0db84600d01797604430e3b2f77c75568cb06010
octavio <ott.html@gmail.com>
2021-08-07 21:20:09 +0000

    feat: ensure email-validator-adapter validates correctly

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,17 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+describe('EmailValidatorAdapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid@mail.com')
+    expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid@mail.com')
+    expect(isValid).toBe(true)
+  })
+})
